b1df4bfd7900e4de90bd22e19deaa5fa77e15607
prats-2311 <prateek.srivastava2311@gmail.com>
2024-07-09 19:05:24 +0530

    with concat limits size

--- cat.js
+++ cat.js
@@ -1,11 +1,24 @@
 var concat = require('concat-stream')
 var http= require('http')
+var through=require('through2')
 var qs = require('querystring')
 var server = http.createServer(function (req,res){
-req.pipe(concat({encoding:'string'},function (body){
+req
+.pipe(counter())
+.pipe(concat({encoding:'string'},onBody))
+function counter(){
+var size=0
+return through(function(buf,enc,next){
+size+=buf.length
+if(size>20){
+next(null,null)
+}else next(null,buf)
+})}
+
+function onBody(body){
 var params=qs.parse(body)
 console.log(params)
 res.end('ok\n')
-}))
+}
 })
 server.listen(6000)
